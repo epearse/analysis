8db0a8a1bb431d321ac6dc84577cff51a24e7690
Erin <161980583+epearse@users.noreply.github.com>
2026-03-02 23:44:35 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,49 +1,13 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "shorttitlelowercase-2",
+  "id": "shorttitlelowercase",
   "level": "1",
-  "url": "shorttitlelowercase-2.html",
-  "type": "Front Matter",
+  "url": "#shorttitlelowercase",
+  "type": "Article",
   "number": "",
-  "title": "Front Matter",
-  "body": "  Author X. Lastname    This is the abstract.   "
-},
-{
-  "id": "section-1",
-  "level": "1",
-  "url": "section-1.html",
-  "type": "Section",
-  "number": "1",
-  "title": "My First Section",
-  "body": " My First Section   Some words    My First Subsection  Some more words  foo   A frog   A nice-looking frog with a longish description.    Bar:   "
-},
-{
-  "id": "subsection-1-4",
-  "level": "2",
-  "url": "section-1.html#subsection-1-4",
-  "type": "Figure",
-  "number": "1.1",
-  "title": "",
-  "body": " A frog   A nice-looking frog with a longish description.   "
-},
-{
-  "id": "section-2",
-  "level": "1",
-  "url": "section-2.html",
-  "type": "Section",
-  "number": "2",
-  "title": "My Additional Section",
-  "body": " My Additional Section  foo   The graph made by TikZ   A 5-cycle     bar  "
-},
-{
-  "id": "figure-tikz-example-diagram",
-  "level": "2",
-  "url": "section-2.html#figure-tikz-example-diagram",
-  "type": "Figure",
-  "number": "2.1",
-  "title": "",
-  "body": " The graph made by TikZ   A 5-cycle    "
+  "title": "Week 9 Quiz",
+  "body": " Week 9 Quiz  Suppose that . Compute .  Note that this is the same as in   "
 }
 ]
 
