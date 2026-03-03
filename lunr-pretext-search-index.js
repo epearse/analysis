57aa7a22ad93d54497124cfc29165a272c047bdd
Erin <161980583+epearse@users.noreply.github.com>
2026-03-03 01:04:20 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,13 +1,22 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "stumblebunny",
+  "id": "quiz-8",
   "level": "1",
-  "url": "#stumblebunny",
-  "type": "Article",
-  "number": "",
-  "title": "Week 9 Qui<span class=\"process-math\">\\(\\scalebox{-1}[1]{z}\\)<\/span>",
-  "body": " Week 9 Qui  Suppose that . Compute .  Note that this is the same as in   "
+  "url": "quiz-8.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Week 8 Quiz",
+  "body": " Week 8 Quiz  Suppose that is continuous and satisfies Prove that .  "
+},
+{
+  "id": "quiz-9",
+  "level": "1",
+  "url": "quiz-9.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Week 9 Quiz",
+  "body": " Week 9 Quiz   Suppose that . Compute .  Note that this is the same as in   "
 }
 ]
 
